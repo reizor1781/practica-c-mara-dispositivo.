e261a5dbd6b1cc9c794fb47713eb3141e37b2352
Carlos Lopez <lopez.carlos.4807@eam.edu.co>
2026-04-22 11:19:51 -0500

    Se corrigio el preview de la imagen

--- app/src/main/java/com/qz/prubacamaraapp/CamaraScreen.kt
+++ app/src/main/java/com/qz/prubacamaraapp/CamaraScreen.kt
@@ -12,7 +12,9 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -24,6 +26,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
@@ -32,58 +35,88 @@ import coil.compose.rememberAsyncImagePainter
 @Composable
 fun CameraScreen() {
     val context = LocalContext.current
-    var imagenUri by remember { mutableStateOf<Uri?>(null) }
-    fun crearUriImagen(context: Context): Uri? {
-        val values = ContentValues().apply {
-            put(
-                MediaStore.Images.Media.DISPLAY_NAME, "foto_${System.currentTimeMillis()}.jpg")
-                    put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
-            put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/MiApp")
-        }
-        return context.contentResolver.insert(
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-            values
-        )
-    }
+
+    var imagenBitmap by remember { mutableStateOf<Bitmap?>(null) }
+    var mensaje by remember { mutableStateOf("") }
+
+
     val launcherCamara = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.TakePicture()
-    ) { success ->
-        if (!success) {
-            imagenUri = null
+        contract = ActivityResultContracts.TakePicturePreview()
+    ) { bitmap ->
+        bitmap?.let {
+            imagenBitmap = it
+            guardarImagen(context, it)
+            mensaje = "Imagen guardada en galería"
         }
     }
+
+
     val launcherPermiso = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestPermission()
     ) { concedido ->
         if (concedido) {
-            imagenUri = crearUriImagen(context)
-            launcherCamara.launch(imagenUri!!)
+            launcherCamara.launch(null)
+        } else {
+            mensaje = "Permiso de cámara denegado"
         }
     }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+
         Button(onClick = {
-            if (ContextCompat.checkSelfPermission(context,
+            if (ContextCompat.checkSelfPermission(
+                    context,
                     Manifest.permission.CAMERA
                 ) == PackageManager.PERMISSION_GRANTED
             ) {
-                imagenUri = crearUriImagen(context)
-                launcherCamara.launch(imagenUri!!)
+                launcherCamara.launch(null)
             } else {
                 launcherPermiso.launch(Manifest.permission.CAMERA)
             }
         }) {
             Text("Tomar Foto")
         }
-        imagenUri?.let {
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+  
+        imagenBitmap?.let { bitmap ->
             Image(
-                painter = rememberAsyncImagePainter(it),
+                bitmap = bitmap.asImageBitmap(),
                 contentDescription = "Foto tomada",
-                modifier = Modifier.size(200.dp)
+                modifier = Modifier.size(250.dp),
+                contentScale = ContentScale.Crop
             )
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        if (mensaje.isNotEmpty()) {
+            Text(text = mensaje)
+        }
+    }
+}
+fun guardarImagen(context: Context, bitmap: Bitmap) {
+    val nombreArchivo = "foto_${System.currentTimeMillis()}.jpg"
+
+    val values = ContentValues().apply {
+        put(MediaStore.Images.Media.DISPLAY_NAME, nombreArchivo)
+        put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+        put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/MiApp")
+    }
+
+    val uri = context.contentResolver.insert(
+        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+        values
+    )
+
+    uri?.let {
+        context.contentResolver.openOutputStream(it)?.use { outputStream ->
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
         }
     }
 }
